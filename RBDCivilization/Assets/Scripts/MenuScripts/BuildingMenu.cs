9626efd2cd2fbaddbf53af10a6b8ff5329eb0ff7
jandrit <al361652@uji.es>
2019-11-08 13:44:06 +0100

    Panel de ciudad hecho
    Como aún no está lo de las casillas, lo he dejado que este de inicio para que se pueda probar, el botón yes y move aún no hace nada pq no esta lo q haría aún.
    
    Más adelante en el proyecto haré que sea más bonito el abrir y cerrar las ventanas con la animación, pero por ahora va guay.

---
+++ RBDCivilization/Assets/Scripts/MenuScripts/BuildingMenu.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingMenu : MonoBehaviour
+{
+    public GameObject buildingInfoMenuUI;
+    public GameObject firstPanelUI;
+    public GameObject surePanelUI;
+
+    public void CloseWindow()
+    {
+        buildingInfoMenuUI.SetActive(false);
+    }
+
+    public void MovePopulation() 
+    {
+        //Aquí será el desplazamiento de población
+    }
+
+    public void LevelUpButton()
+    {
+        firstPanelUI.SetActive(false);
+        surePanelUI.SetActive(true);
+    }
+
+    public void YesButton()
+    {
+        firstPanelUI.SetActive(true);
+        surePanelUI.SetActive(false);
+        //Aquí hacer la función de subir de nivel y tal
+    }
+
+    public void NoButton()
+    {
+        firstPanelUI.SetActive(true);
+        surePanelUI.SetActive(false);
+    }
+}
