8f62b2e150237bfb65805bc0f05f6c13fc98be97
AXBNBS <44948868+AXBNBS@users.noreply.github.com>
2019-11-09 04:15:14 +0100

    Creación de las celdas en el terreno.
    A ver falta el modelo del héxagono y algunos ajustes pero sí.

---
+++ RBDCivilization/Assets/Scripts/Grid/Grid.cs
@@ -0,0 +1,90 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Grid : MonoBehaviour
+{
+    [SerializeField] private Transform hexagonPfb, terrainTrf;
+    [SerializeField] private float gridWth, gridHgt, gap;
+    private float hexagonWth, hexagonHgt;
+    private Vector3 startPos;
+
+
+    // We initialize some variables and add the gap to the hexagons's with and height, we calculate the starting position of the grid, and we finally create it.
+    private void Start ()
+    {
+        gridWth = terrainTrf.localScale.x * 2;
+        gridHgt = terrainTrf.localScale.z * 2;
+        hexagonWth = hexagonPfb.localScale.x;
+        hexagonHgt = hexagonPfb.localScale.z;
+
+        AddGap ();
+        StartPosition ();
+        CreateGrid ();
+    }
+
+
+    // We add the gap value we've set to the hexagons's with and height.
+    private void AddGap () 
+    {
+        hexagonWth += hexagonWth * gap;
+        hexagonHgt += hexagonHgt * gap;
+    }
+
+
+    // We define the starting position where the first hexagon will be placed.
+    private void StartPosition () 
+    {
+        float offset = 0;
+
+        if (gridHgt / 2 % 2 != 0) 
+        {
+            offset = hexagonWth / 2;
+        }
+
+        float x = -hexagonWth * (gridWth / 2) - offset;
+        float z = +hexagonHgt * 0.75f * (gridHgt / 2);
+
+        startPos = new Vector3 (x, 0, z);
+    }
+
+
+    // Get an hexagon's world position from its position on the grid.
+    Vector3 CalculateWorldPosition (Vector2 gridPos) 
+    {
+        float offset = 0;
+        if (gridPos.y % 2 != 0) 
+        {
+            offset = hexagonWth / 2;
+        }
+
+        float x = startPos.x + gridPos.x * hexagonWth + offset;
+        float z = startPos.z - gridPos.y * hexagonHgt * 0.75f;
+
+        return new Vector3 (x, 0, z);
+    }
+
+
+    // We instantiate every hexagon and put it in its corresponding position on the grid.
+    private void CreateGrid () 
+    {
+        int hexagonCnt = 1;
+
+        for (int y = 0; y < gridHgt; y += 1) 
+        {
+            for (int x = 0; x < gridWth; x += 1)
+            {
+                Transform hexagon = Instantiate (hexagonPfb) as Transform;
+                Vector2 gridPos = new Vector2 (x, y);
+
+                hexagon.position = CalculateWorldPosition (gridPos);
+                hexagon.parent = this.transform;
+                hexagon.name = "Hexagon" + hexagonCnt;
+                hexagonCnt += 1;
+            }
+        }
+    }
+}
---
+++ RBDCivilization/Assets/Scripts/Grid/GridManager.cs
@@ -0,0 +1,120 @@
+﻿
+/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+namespace SA
+{
+    public class GridManager : MonoBehaviour
+    {
+        public Vector3 extents;
+        
+        [SerializeField] private float xzScale, yScale;
+        private Node[,] grid;
+        private int maxX, maxZ, maxY;
+        private Vector3 minPos;
+        private List<Vector3> nodeViz;
+
+
+        // We initialize some variables.
+        private void Awake ()
+        {
+            nodeViz = new List<Vector3> ();
+            extents = new Vector3 (0.8f, 0.8f, 0.8f);
+            xzScale = 1.5f;
+            yScale = 2;
+
+            ReadLevel ();
+        }
+
+
+        // Update is called once per frame.
+        private void Update ()
+        {
+
+        }
+
+
+        //
+        private void OnDrawGizmos ()
+        {
+            Gizmos.color = Color.green;
+
+            for (int i = 0; i < nodeViz.Count; i += 1)
+            {
+                Gizmos.DrawWireCube (nodeViz[i], extents);
+            }
+        }
+
+
+        //
+        private void ReadLevel () 
+        {
+            GridPosition[] gridPos = GameObject.FindObjectsOfType<GridPosition> ();
+            float minX = float.MaxValue;
+            float maxX = float.MinValue;
+            float minZ = minX;
+            float maxZ = maxX;
+
+            for (int i = 0; i < gridPos.Length; i += 1) 
+            {
+                Transform t = gridPos[i].transform;
+
+                if (t.position.x < minX) 
+                {
+                    minX = t.position.x;
+                }
+                if (t.position.x > maxX) 
+                {
+                    maxX = t.position.x;
+                }
+                if (t.position.z < minZ) 
+                {
+                    minX = t.position.z;
+                }
+                if (t.position.z > maxZ) 
+                {
+                    maxZ = t.position.z;
+                }
+            }
+
+            int posX = Mathf.FloorToInt ((maxX - minX) / xzScale);
+            int posZ = Mathf.FloorToInt ((maxZ - minZ) / xzScale);
+
+            minPos = Vector3.zero;
+            minPos.x = minX;
+            minPos.z = minZ;
+
+            CreateGrid (posX, posZ);
+        }
+
+
+        //
+        private void CreateGrid (int posX, int posZ) 
+        {
+            grid = new Node[posX, posZ];
+
+            for (int x = 0; x < posX; x += 1) 
+            {
+                for (int z = 0; z < posZ; z += 1) 
+                {
+                    Node n = new Node ();
+
+                    n.x = x;
+                    n.z = z;
+
+                    Vector3 transformPos = minPos;
+
+                    transformPos.x += x * xzScale;
+                    transformPos.z += z * xzScale;
+                    n.worldPos = transformPos;
+                    grid[x, z] = n;
+
+                    nodeViz.Add (n.worldPos);
+                }
+            }
+        }
+    }
+}*/
