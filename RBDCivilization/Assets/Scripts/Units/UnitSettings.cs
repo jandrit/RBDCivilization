a1abc7367c2706320ff846ef3db621bb49e330a8
marklg5 <43933460+marklg5@users.noreply.github.com>
2019-11-10 20:24:40 +0100

    Clases de unidades creadas
    Como los edificios, tienen assets creados uno por cada unidad con sus respectivos datos listos para añadirse al prefab correspondiente junto con un script principal de unidad que obtenga los datos mediante los assets

---
+++ RBDCivilization/Assets/Scripts/Units/UnitSettings.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName ="Unit Setting")]
+public class UnitSettings : ScriptableObject
+{
+    public float attack;
+    public float speed;
+    public float defense;
+    public string race;
+    public string occupation;
+    public int stores;
+}
